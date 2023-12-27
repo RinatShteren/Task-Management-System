3becb461fde3518ce143e08cb0c5a64e23cdb968
RinatShteren <rinatshteren@gmail.com>
2023-12-27 14:14:28 +0200

    initial code +welcome functions

---
+++ stage0/Program2159.cs
@@ -0,0 +1,26 @@
+﻿
+namespace stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome2159();
+            welcome6722();
+            Console.ReadKey();
+        }
+
+        private static void welcome6722()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void welcome2159()
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console application", name);
+        }
+    }
+}
+ 

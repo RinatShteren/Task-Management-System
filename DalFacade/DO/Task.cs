87efa4b142e4e623a6a4e5bac6c847c408b5c95b
Orgilad <orestercohen@gmail.com>
2024-01-02 21:45:45 +0200

    ישות משימה

--- DalFacade/DO/Task.cs
+++ DalFacade/DO/Task.cs
@@ -1,7 +1,41 @@
 ﻿
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace DO;
+/// <summary>
+/// Task Entity represents a Task with all its props
+/// </summary>
+/// <param name="Id">Unique ID number</param>
+/// <param name="NickName"></param>
+/// <param name="Description">A text detailing the task</param>
+/// <param name="MileStone"></param>
+/// <param name="CreationDate">The time when the task was created by the manager</param>
+/// <param name="EstimatedDate">Planned date for work to begin</param>
+/// <param name="StartDate">Date of actual start of work on the assignment</param>
+/// <param name="NumOfDays">The number of working days required to complete the task</param>
+/// <param name="DeadLine">The latest possible date on which the completion of the task will not cause the project to fail</param>
+/// <param name="FinishtDate">Actual work completion date</param>
+/// <param name="Product">The results or items provided at the end of the task</param>
+/// <param name="Remarks"></param>
+public record Task
+(
+    int Id,                             
+    string? NickName = null,            
+    string? Description = null,         
+    bool MileStone = false,              
+    DateTime? CreationDate= null,         
+    DateTime? EstimatedDate = null,        
+    DateTime? StartDate = null,         
+    int? NumOfDays=null,                
+    DateTime? DeadLine = null,          
+    DateTime? FinishtDate = null,       
+    string? Product=null,               
+    string? Remarks = null
 
-internal class Task
+
+)
 {
-
+    public Task() : this(0) { }
+    public Task()
 }

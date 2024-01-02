2cc157ff2fd8e3308d3f4765ac13254db27b8f9d
RinatShteren <rinatshteren@gmail.com>
2024-01-02 19:59:05 +0200

    starting the Tast.cs

--- DalFacade/DO/Engineer.cs
+++ DalFacade/DO/Engineer.cs
@@ -1,15 +1,26 @@
 ﻿
 namespace DO;
 
+///
+///Enegeneer entity represent an engeneer with all its props
+////
 public record Engineer
 (
     int Id,
-    string? Namel = null,
+    string? Name = null,
     string? Email = null,
     string? Experience = null,
-    int Cost =0 
+    int Cost = 0
 )
 {
-    public Engineer():this(0) { }
-    //public Engineer() { }
-}
+    public Engineer() : this(0) { }
+    //*public Engineer(int id, string? name, string? email, string? experience, int cost)
+    //{ 
+     //Id = id;
+    //Name = name;
+    //Email = email;
+    //Experience = experience;
+    //Cost = cost;
+    }
+///}
+

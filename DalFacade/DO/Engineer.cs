03b8c55102873325e49d99053934bb2c08eae60e
RinatShteren <rinatshteren@gmail.com>
2024-01-02 09:15:16 +0200

    bilding firstrecord on DO

---
+++ DalFacade/DO/Engineer.cs
@@ -0,0 +1,15 @@
+﻿
+namespace DO;
+
+public record Engineer
+(
+    int Id,
+    string? Namel = null,
+    string? Email = null,
+    string? Experience = null,
+    int Cost =0 
+)
+{
+    public Engineer():this(0) { }
+    //public Engineer() { }
+}
--- README.md
+++ README.md
@@ -2,5 +2,3 @@ hii how are you?
 
 # dotNet5784_2159_6722
 c# project - JCT
-hii rinat1
-hi or1

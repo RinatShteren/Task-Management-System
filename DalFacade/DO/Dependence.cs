513717dbb6788b87ee95f8f412e580ec1a76d4ba
Orgilad <orestercohen@gmail.com>
2024-01-03 23:50:39 +0200

    שלב 3 גמור

--- DalFacade/DO/Dependence.cs
+++ DalFacade/DO/Dependence.cs
@@ -1,7 +1,17 @@
 ﻿
 namespace DO;
-
+/// <summary>
+/// Specifies which task can only be performed after the completion of a previous task
+/// </summary>
+/// <param name="DependenceId">Unique ID number of the Dependence</param>
+/// <param name="PendingTaskId">ID number of pending task</param>
+/// <param name="PreviousTaskId">Previous assignment ID number</param>
 public record Dependence
 (
-
-);
+    int DependenceId,
+    int PendingTaskId,
+    int PreviousTaskId
+)
+{
+    public Dependence() : this(0,0,0) { }
+}

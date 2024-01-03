fb1f500a9dcd736a87f991c5a812c96d8eab16f0
Orgilad <orestercohen@gmail.com>
2024-01-03 14:04:14 +0200

    הגדרת ישויות

--- DalFacade/DO/Engineer.cs
+++ DalFacade/DO/Engineer.cs
@@ -1,26 +1,22 @@
 ﻿
 namespace DO;
-
-///
-///Enegeneer entity represent an engeneer with all its props
-////
+/// <summary>
+/// Enegeneer entity represent an engeneer with all its props
+/// </summary>
+/// <param name="Id"></param>
+/// <param name="Name"></param>
+/// <param name="Email"></param>
+/// <param name="Experience"></param>
+/// <param name="Cost"></param>
 public record Engineer
 (
     int Id,
     string? Name = null,
     string? Email = null,
-    string? Experience = null,
+    EngineerLevel Level,
     int Cost = 0
 )
 {
     public Engineer() : this(0) { }
-    //*public Engineer(int id, string? name, string? email, string? experience, int cost)
-    //{ 
-     //Id = id;
-    //Name = name;
-    //Email = email;
-    //Experience = experience;
-    //Cost = cost;
-    }
-///}
+}
 

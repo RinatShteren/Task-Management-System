e2d71a0db814da2520f1a7bba1e139956c6fcdf6
RinatShteren <rinatshteren@gmail.com>
2024-01-04 14:50:13 +0200

    DelApi class interface- part4

---
+++ DalFacade/DelApi/IDependence.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DelApi;
+using DO;
+
+internal interface IDependence
+{
+    int Create(Dependence item); //Creates new entity object in DAL
+    Dependence? Read(int id); //Reads entity object by its ID
+    List<Dependence?> ReadAll(); //stage 1 only, Reads all entity objects
+    void Update(Dependence item); //Updates entity object
+    void Delete(int id); //Deletes an object by is Id
+}

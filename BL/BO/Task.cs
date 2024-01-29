3dc069c91f00368d0885d6d0313724df37178c00
RinatShteren <rinatshteren@gmail.com>
2024-01-29 13:57:58 +0200

    stage7

---
+++ BL/BO/Task.cs
@@ -0,0 +1,21 @@
+﻿
+namespace BO;
+
+public class Task
+{
+    public int TaskId { get; init; }
+    public string? NickName { get; set; }
+    public string? Description { get; set; }
+    public bool MileStone { get; set; }
+    public DateTime? CreationDate { get; set; }
+    public DateTime? EstimatedDate { get; set; }
+    public DateTime? StartDate { get; set; }
+    public int? NumOfDays { get; set; }
+    public DateTime? DeadLine { get; set; }
+    public DateTime? FinishtDate { get; set; }
+    public string? Product { get; set; }
+    public string? Remarks { get; set; }
+    public int EngineerId { get; set; }
+    public EngineerLevel? RequiredLevel { get; set; }
+
+}
---
+++ BL/BlApi/ITask.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BlApi;
+
+public interface ITask
+{
+    public int Create(BO.Task boStudent);
+
+    public BO.Task? Read(int id);
+    public IEnumerable<BO.Task?> ReadAll();
+    public void Delete(int id);
+}

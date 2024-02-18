9cddacd70977b6d60770e4291f09ace3d05ad8ca
RinatShteren <rinatshteren@gmail.com>
2024-02-18 13:04:49 +0200

    stage 4 - fix

--- BL/BO/EngineerInList.cs
+++ BL/BO/EngineerInList.cs
@@ -5,6 +5,6 @@ public class EngineerInList
 {
     public int Id { get; init; }
     public string? Name { get; set; }
-    public int CurrentYear { get; set; }
+    public int CurrentYear { get; set; } 
 
 }
